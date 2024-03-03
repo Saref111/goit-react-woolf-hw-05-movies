d218ba5ad762087447fc524a01d23b2fce934e2a
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-03-03 14:58:26 +0200

    added film details

--- src/pages/MoviesDetails/MoviesDetails.jsx
+++ src/pages/MoviesDetails/MoviesDetails.jsx
@@ -1,7 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { fetchMovieDetails } from 'services/api';
+import css from './MoviesDetails.module.scss';
+
 export const MoviesDetails = () => {
-    return (
-        <div>
-            <h1>MoviesDetails</h1>
-        </div>
-    );
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    if (!movieId) return;
+    const fetchMovie = async () => {
+      const response = await fetchMovieDetails(movieId);
+      setMovie(response.data);
+    };
+
+    try {
+      fetchMovie();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [movieId]);
+
+  return (
+    <div>
+      {movie && (
+        <section className={css.section}>
+          <img src={'https://image.tmdb.org/t/p/w600_and_h900_bestv2/' + movie.backdrop_path} alt={movie.title} width={600} height={900}/>
+          <div>
+            <h1>{movie.title}</h1>
+            <p>User score: {movie.popularity}</p>
+            <h2>Overview</h2>
+            <p>{movie.overview}</p>
+            <h2>Genres:</h2>
+            <p>{movie.genres.map((it) => it.name).join(', ')}</p>
+          </div>
+        </section>
+      )}
+      <Outlet />
+    </div>
+  );
 };
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const API_KEY = 'e13df1ab94334c8c1ab68ca9010891e0';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
-axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.baseURL = BASE_URL;
 axios.defaults.params = {
   api_key: API_KEY,
 };
@@ -10,3 +11,7 @@ axios.defaults.params = {
 export const fetchTrendingMovies = async () => {
   return await axios.get(`trending/movie/day`);
 };
+
+export const fetchMovieDetails = async (movieId) => {
+  return await axios.get(`movie/${movieId}`);
+}
