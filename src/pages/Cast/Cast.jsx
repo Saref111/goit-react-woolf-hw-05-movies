c8e103643a5fbc0943c09de7c3d019bbf498005e
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-03-03 15:23:56 +0200

    added casts

--- src/pages/Cast/Cast.jsx
+++ src/pages/Cast/Cast.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMovieCast } from 'services/api';
+import css from './Cast.module.scss';
+
+const defaultImg = 'https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700'
 
 export const Cast = () => {
     const [cast, setCast] = useState([]);
@@ -18,5 +21,24 @@ export const Cast = () => {
             console.log(error);
         }
     }, [movieId]);
-    return <h1>Cast</h1>;
+    return (
+        <div>
+            <ul className={css.castList}>
+                {cast?.map((actor) => (
+                    <li className={css.castItem} key={actor.id}>
+                        <img
+                            src={
+                                actor.profile_path ? 'https://image.tmdb.org/t/p/w200' + actor.profile_path : defaultImg
+                            }
+                            alt={actor.name}
+                            width={100}
+                            height={150}
+                        />
+                        <p>{actor.name}</p>
+                        <p>Character: {actor.character}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
